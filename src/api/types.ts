ca37db2a49b0a623ab79bc7a3de920fb033b874c
PriyankaDas856 <priya18bkj@gmail.com>
2026-08-28 11:00:19 +0530

    feat: add mock grocery catalogue and product types

---
+++ src/api/types.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string
+  name: string
+  category: string
+  subcategory: string
+  price: number
+  unit: string
+  stock: number
+  image: string
+  rating: number
+}
